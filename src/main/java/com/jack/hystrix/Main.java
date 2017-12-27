b523eedb3cd16fabf131c2f7db0c67a425154ac3
hefei <hefei@youzan.com>
2017-12-27 16:36:02 +0800

    update

--- src/main/java/com/jack/hystrix/Main.java
+++ src/main/java/com/jack/hystrix/Main.java
@@ -1,20 +1,30 @@
 package com.jack.hystrix;
 
 import rx.Observable;
-import rx.functions.Action1;
+import rx.Observer;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    GetNameCommand gnc = new GetNameCommand("abc");
-    System.out.println(gnc.execute());
+//    GetNameCommand gnc = new GetNameCommand("abc");
+//    System.out.println(gnc.execute());
 
     GetNameObservableCommand gnoc = new GetNameObservableCommand("bcd");
     Observable o = gnoc.construct();
 
-    o.subscribe(new Action1<String>() {
-      public void call(String o) {
-        System.out.println("Action1:" + o);
+    o.subscribe(new Observer<String>() {
+      public void onCompleted() {
+        System.out.println("completed");
+      }
+
+      public void onError(Throwable throwable) {
+        System.out.println("on error: " + throwable.getMessage());
+      }
+
+      public void onNext(String o) {
+        System.out.println("msg:" + o);
       }
     });
+
+    o.subscribe((v) -> {System.out.println("function:" + v);});
   }
 }
